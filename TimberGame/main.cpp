d25261e476222c532497d587e2a4b5e55421d7ed
renan-77 <renan.deoliveiramonteiro@student.griffith.ie>
2019-12-27 01:45:09 +0000

    Initial Commit

---
+++ TimberGame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TimberGame
+//
+//  Created by Renan Monteiro on 27/12/2019.
+//  Copyright © 2019 Renan Monteiro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
